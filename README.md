ad85c3116857fc932fcb83e4777cd57a018597d8
Samarium150 <28302241+Samarium150@users.noreply.github.com>
2026-09-16 16:32:13 -0600

    feat: create lab 4 challenge

---
+++ README.md
@@ -0,0 +1,3 @@
+# CMPUT 350 Lab 4 Challenge
+
+[Instructions](https://uofa-cmput350.github.io/materials/lab/4/challenge.html)
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <cstdint>  // uint64_t, uint32_t
+
+// Make sure you include an explanation for all challenge problems attempted!
+
+// C1
+uint64_t expand3(uint64_t input) { return 0; }
+
+// C2
+uint64_t expand(uint64_t input, uint32_t scale) { return 0; }
+
+// with templates...
+template <uint32_t Scale>
+uint64_t expand(uint64_t input) {
+    return 0;
+}
+
+int main() {
+    // test here...
+    return 0;
+}
